f8751fb981b03f791a7c5245eb2064a3316eda32
Usman Munir <usman.munir@devparagon.com>
2025-01-20 17:14:23 +0500

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html"
-  ],
+  content: ["./index.html"],
   theme: {
     extend: {
       fontFamily: {
-        urbanist: ['Urbanist', 'sans-serif'], // Add the Urbanist font here
+        urbanist: ["Urbanist", "sans-serif"],
       },
-      colors: {
-        primary: {
-          DEFAULT: '#44A0E4',
-          100: '#44A0E4',
-          200: '#44A0E4'
-        },
-      'custom-blue': '#44A0E4',
     },
   },
-  },
   plugins: [],
-}
-
+};
